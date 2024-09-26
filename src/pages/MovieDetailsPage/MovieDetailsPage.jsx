b5b66c1aba844f84e891e1bcc21db6e4d5560a69
Mariya Buzhava <bujava.masha@gmail.com>
2024-09-26 23:53:00 +0300

    moviedetailspage css

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,7 +1,7 @@
-// import c from './MovieDetailsPage.module.css'
+import c from "./MovieDetailsPage.module.css";
 
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { fetchTrendingMoviesById } from "../../servers/api";
 
 const MovieDetailsPage = () => {
@@ -21,23 +21,42 @@ const MovieDetailsPage = () => {
   }
 
   return (
-    <div>
-      Movie Details for ID: {movieId}
-      {movie.poster_path && (
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-        />
-      )}
-      <h3>{movie.title}</h3>
-      <p>
-        <strong>Overview:</strong> {movie.overview}
-      </p>
-      <p>
-        <strong>Genres:</strong>
-        {movie.genres.map((genre) => genre.name).join(", ")}
-      </p>
-    </div>
+    <>
+      <Link to="/" className={c.link}>
+        ← Go back
+      </Link>
+      <div className={c.container}>
+        <div className={c.backdrop}>
+          {movie.backdrop_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt={movie.title}
+            />
+          )}
+        </div>
+
+        <div className={c.poster}>
+          {movie.poster_path && (
+            <img
+              className={c.posterImage}
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+          )}
+        </div>
+
+        <div className={c.textContainer}>
+          <h3>{movie.title}</h3>
+          <p>
+            <strong>Overview: </strong> {movie.overview}
+          </p>
+          <p>
+            <strong>Genres: </strong>
+            {movie.genres.map((genre) => genre.name).join(", ")}
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
